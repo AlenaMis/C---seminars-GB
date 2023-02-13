6b018b35feab044c69d78d4d0998cf3b945d5a17
Alena <alenamis90@mail.ru>
2023-02-13 06:12:45 +0300

    Задача 1

---
+++ Задача1/Program.cs
@@ -0,0 +1,22 @@
+﻿ // Напишите программу, которая на вход приниает трехзначное число 
+ // и на выходе показывает вторую цифру этого числа.
+ // 456 -> 5
+ // 782 -> 8
+ // 918 -> 1
+
+Console.Clear();
+
+int value;
+
+value = 258;
+
+if (value > 99 && value < 1000)
+{
+    int res;
+    res = (value / 10) % 10;
+    Console.WriteLine(res);
+}
+else
+{
+    Console.WriteLine("ERROR");
+}
