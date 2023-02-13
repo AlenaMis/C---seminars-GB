0a10aceb63c25e6bb2a5f34b48a0a1c2696c24ae
Alena <alenamis90@mail.ru>
2023-02-13 15:28:17 +0300

    Задача 4

---
+++ Задача4/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает одно число (N)
+// а на выходе показывает все числа в промежутке  от -N до N
+// 4 -> -4, -3, -2, -1, 0, 1, 2, 3, 4
+// 2 -> -2, -1, 0, 1, 2
+
+Console.Clear();
+
+int N = 4;
+        
+int currentIndex = -N; // -N это тоже самое, что -1*N
+
+while (currentIndex <= N)
+{
+    Console.Write(currentIndex + " ");
+    currentIndex = currentIndex + 1;
+}
